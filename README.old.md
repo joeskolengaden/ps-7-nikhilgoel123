f3628e0e81316b22927231b2ab1d724e38c8ebb3
Nikhil GOEL <Nikhilgoel@Nikhils-MacBook-Pro-2.local>
2018-11-15 16:32:07 -0800

    Updates

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# ps-7-nikhilgoel123
-Problem Set 7 Info 340
---
+++ precache-manifest.5fd344e044fc0ff8e5eecdb1117ee17c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e61f303dda4b5fec1915686811148c44",
+    "url": "/project-ps-7-nikhilgoel123/static/media/wandering.e61f303d.jpg"
+  },
+  {
+    "revision": "21f85b689e630235cf2e5b435ba5491d",
+    "url": "/project-ps-7-nikhilgoel123/static/media/burger.21f85b68.jpg"
+  },
+  {
+    "revision": "43357614570de8b05c32",
+    "url": "/project-ps-7-nikhilgoel123/static/js/runtime~main.43357614.js"
+  },
+  {
+    "revision": "1a4ae3878aedfb841550",
+    "url": "/project-ps-7-nikhilgoel123/static/js/main.1a4ae387.chunk.js"
+  },
+  {
+    "revision": "92bc93635a3ea8e11847",
+    "url": "/project-ps-7-nikhilgoel123/static/js/1.92bc9363.chunk.js"
+  },
+  {
+    "revision": "1a4ae3878aedfb841550",
+    "url": "/project-ps-7-nikhilgoel123/static/css/main.96eafa19.chunk.css"
+  },
+  {
+    "revision": "92bc93635a3ea8e11847",
+    "url": "/project-ps-7-nikhilgoel123/static/css/1.5f1f4ca7.chunk.css"
+  },
+  {
+    "revision": "d75411ae52140639bb313ce623e3025c",
+    "url": "/project-ps-7-nikhilgoel123/index.html"
+  }
+];
